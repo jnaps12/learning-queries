16415989051f530e1f7069bcbd90d9c8290d462f
Junior Pinheiro <junior.main7@gmail.com>
2022-12-05 14:19:28 -0300

    making question

--- src/Pages/Question.jsx
+++ src/Pages/Question.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { Container, Alert } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import Figure from 'react-bootstrap/Figure';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+import '../assets/style/Question.css'
 
 export function Question({ data }) {
   const { difficulty, id } = useParams();
+  const now = (1/3) * 100;
+  return (
+    <Container>
+      <h1 align="center" className="mt-5">
+        Select 1
+      </h1>
+      <ProgressBar now={now} animated label={`1/3`} />
 
-  console.log(difficulty);
-  console.log(id);
+      <div className="question-box mt-5">
+        <Alert variant="primary" align="center">
+          Observe o Schema abaixo e organize os bloquinhos para completar a
+          Query fazendo o que se pede.
+        </Alert>
+        <Figure align="center" className="question-image">
+          <Figure.Image alt="qualuqer" src="/exemplo.png" />
+          <Figure.Caption>
+            Busque todos os dados da tabela “usuarios” :
+          </Figure.Caption>
+        </Figure>
 
-  return <div>Question</div>;
+        <div className="droppable">
+
+        </div>
+        <div className="draggable">
+
+        </div>
+      </div>
+    </Container>
+  );
 }
