e94b403b5f907cb4de29d2ea649c96283e6470db
Junior Pinheiro <junior.main7@gmail.com>
2022-12-19 13:54:39 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,7 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { Main } from './layouts/Main';
 
-const privateRoutes = [
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/questions',
-        element: <BoardQuestions />,
-        children: [
-          {
-            path: '/questions/easy',
-            element: <EasyQuestions />,
-          },
-          {
-            path: '/questions/intermediary',
-            element: <IntermediaryQuestions />,
-          },
-          {
-            path: '/questions/hard',
-            element: <HardQuestions />,
-          },
-        ],
-      },
-      {
-        path: '/questions/:difficulty/:groupid',
-        element: <QuestionGroupPage />,
-        children: [
-          {
-            path: '/questions/:difficulty/:groupid/question/:questionid',
-            element: <Question />,
-          },
-        ],
-      },
-    ],
-  },
-];
+function App() {
 
   return (
     <>
